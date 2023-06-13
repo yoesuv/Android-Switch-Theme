9bc651796ce0c01ba5d586ad01ac156f260bbe3d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-13 09:20:29 +0700

    :sparkles: switch theme

--- app/src/main/java/com/yoesuv/switchtheme/MainActivity.kt
+++ app/src/main/java/com/yoesuv/switchtheme/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yoesuv.switchtheme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import com.yoesuv.switchtheme.databinding.ActivityMainBinding
 
@@ -14,11 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         setupToolbar()
+        setupSwitch()
     }
 
     private fun setupToolbar() {
         setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun setupSwitch() {
+        binding.switchTheme.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
 
 }
