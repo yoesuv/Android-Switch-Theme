dfedc5e812ebf211b28f2762d9bbeaa32a96564a
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-06-13 14:18:48 +0700

    :sparkles: dialog exit app

--- app/src/main/java/com/yoesuv/switchtheme/MainActivity.kt
+++ app/src/main/java/com/yoesuv/switchtheme/MainActivity.kt
@@ -2,8 +2,10 @@ package com.yoesuv.switchtheme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.yoesuv.switchtheme.databinding.ActivityMainBinding
 
 const val isDark = "isDarkTheme"
@@ -12,12 +14,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var dialogExit: AlertDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         setupToolbar()
         setupSwitch()
+        setupButton()
     }
 
     private fun setupToolbar() {
@@ -38,6 +43,23 @@ class MainActivity : AppCompatActivity() {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                 MyApp.prefHelper?.setBoolean(isDark, false)
             }
+        }
+    }
+
+    private fun setupButton() {
+        val dialogBuilder = MaterialAlertDialogBuilder(this, R.style.DialogTheme)
+            .setTitle(getString(R.string.exit).uppercase())
+            .setMessage(R.string.exit_message)
+            .setPositiveButton(android.R.string.ok) { _, _ ->
+                dialogExit?.dismiss()
+                finish()
+            }
+            .setNegativeButton(android.R.string.cancel) { _, _ ->
+                dialogExit?.dismiss()
+            }
+        dialogExit = dialogBuilder.create()
+        binding.btnExit.setOnClickListener {
+            dialogExit?.show()
         }
     }
 
